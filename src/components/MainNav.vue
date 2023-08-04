7466daa290742a364cf12c4a21f941966ec58495
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-08-03 23:30:40 -0600

    Update MainNav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,11 +1,21 @@
 <script>
+import ActionButton from '@/components/ActionButton.vue'
+
 export default {
   name: 'MainNav',
+  components: { ActionButton },
   data() {
     return {
       company: 'ÁC Careers',
       url: '/',
-      menuItems: ['Teams', 'Locations', 'Life at ÁC Corp', 'How we hire', 'Students', 'Jobs']
+      menuItems: [
+        'Teams',
+        'Locations',
+        'Life at ÁC Corp',
+        'How we hire',
+        'Students',
+        'Jobs'
+      ]
     }
   }
 }
@@ -14,16 +24,26 @@ export default {
 <template>
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
-      <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
+      <div
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+      >
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
 
         <nav class="ml-12 h-full">
           <ul class="flex h-full list-none">
-            <li v-for="menuItem in menuItems" :key="menuItem" class="ml-9 h-full first:ml-0">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="ml-9 h-full first:ml-0"
+            >
               <a class="flex h-full items-center py-2.5" href="">{{ menuItem }}</a>
             </li>
           </ul>
         </nav>
+
+        <div class="ml-auto flex h-full items-center">
+          <ActionButton />
+        </div>
       </div>
     </div>
   </header>
