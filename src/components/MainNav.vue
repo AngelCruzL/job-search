d549f5cf1a779ec7968722b3f11b6a6ca652ff63
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-08-17 22:10:21 -0600

    Refactor styles definition approach for the ActionButton

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -53,7 +53,7 @@ export default {
 
         <div class="ml-auto flex h-full items-center">
           <ProfileImage v-if="isLoggedIn" />
-          <ActionButton v-else :is-primary="false" text="Sign In" @click="loginUser" />
+          <ActionButton v-else text="Sign In" type="primary" @click="loginUser" />
         </div>
       </div>
     </div>
