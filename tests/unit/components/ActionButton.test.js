a8be8dc1c09fd3761f8b97186bb5de06c8d2a9c0
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-08-18 05:51:06 -0600

    Test css classes apply on ActionButton component

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/vue'
+
+import ActionButton from '@/components/ActionButton.vue'
+
+describe('ActionButton', () => {
+  it('should renders text', () => {
+    render(ActionButton, {
+      props: {
+        text: 'Click me',
+        type: 'primary'
+      }
+    })
+    const button = screen.getByRole('button', { name: /click me/i })
+
+    expect(button).toBeInTheDocument()
+  })
+
+  it('should apply the secondary button styles if the button is secondary type', () => {
+    render(ActionButton, {
+      props: {
+        text: 'Click me',
+        type: 'secondary'
+      }
+    })
+    const button = screen.getByRole('button', { name: /click me/i })
+
+    expect(button).toHaveClass('secondary')
+  })
+})
