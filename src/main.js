93deb5d03806f378d7437a4a8c629fc139893002
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-08-18 08:54:17 -0600

    Add font awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import App from '@/App.vue'
 
-createApp(App).mount('#app')
+import './assets/main.css'
+
+library.add(faSearch)
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
