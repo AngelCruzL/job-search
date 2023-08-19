c8801790e38f7e4ee4f0f6df1bfa0604813ac28b
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-08-18 22:05:38 -0600

    Add JobSearchForm component layout

---
+++ src/components/JobSearchForm.vue
@@ -0,0 +1,50 @@
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import ActionButton from '@/components/ActionButton.vue'
+
+export default {
+  name: 'JobSearchForm',
+  components: { ActionButton, FontAwesomeIcon }
+}
+</script>
+
+<template>
+  <form
+    class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+
+    <div class="flex h-full flex-1 flex-nowrap text-base font-light">
+      <div class="relative flex h-full flex-1 items-center pr-3">
+        <label class="absolute -top-10 left-0" for="role">Role</label>
+        <input
+          id="role"
+          class="w-full text-lg font-normal focus:outline-none"
+          placeholder="Software engineer"
+          type="text"
+        />
+      </div>
+
+      <span
+        class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
+      >
+        in
+      </span>
+
+      <div class="relative flex h-full flex-1 items-center pl-3">
+        <label class="absolute -top-10 left-0" for="location">Where?</label>
+        <input
+          id="location"
+          class="w-full text-lg font-normal focus:outline-none"
+          placeholder="CDMX"
+          type="text"
+        />
+      </div>
+    </div>
+
+    <action-button class="rounded-r-3xl" text="Search" type="secondary" />
+  </form>
+</template>
+
+<style scoped></style>
--- src/components/PageHero.vue
+++ src/components/PageHero.vue
@@ -1,25 +1,27 @@
 <script>
 import PageHeadline from '@/components/PageHeadline.vue'
+import JobSearchForm from '@/components/JobSearchForm.vue'
 
 export default {
   name: 'PageHero',
-  components: { PageHeadline }
+  components: { JobSearchForm, PageHeadline }
 }
 </script>
 
 <template>
   <main>
-    <section class="flex h-screen flex-col bg-yellow-200 pb-20 pt-10">
+    <section class="flex h-screen flex-col pb-20 pt-10">
       <div class="grid grid-cols-12">
-        <div class="col-span-1 col-start-1 border border-blue-700">1 column</div>
+        <div class="col-span-1 col-start-1"></div>
 
-        <div class="col-span-5 col-start-2 border border-blue-700">
+        <div class="col-span-5 col-start-2">
           <page-headline />
+          <job-search-form />
         </div>
 
-        <div class="col-span-5 col-start-7 border border-blue-700">1 column</div>
+        <div class="col-span-5 col-start-7"></div>
 
-        <div class="col-span-1 col-start-12 border border-blue-700">1 column</div>
+        <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
   </main>
